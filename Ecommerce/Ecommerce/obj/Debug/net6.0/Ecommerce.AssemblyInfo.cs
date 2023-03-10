500145480e444990c2ce207f877ef1962c4d88cf
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-03-10 08:17:19 +0200

    add template add changes some styles

--- Ecommerce/Ecommerce/obj/Debug/net6.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/Ecommerce/obj/Debug/net6.0/Ecommerce.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-Ecommerce-17b2b74e-7a88-427f-8495-5408646f939d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
